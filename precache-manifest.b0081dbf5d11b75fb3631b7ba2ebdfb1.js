14c512e09b8c40cc0c9ba69f362b93324a746775
AnandaPadmanabhanVT <anandapadmanabhanvt@gmail.com>
2020-09-17 12:33:46 +0530

    Updates

---
+++ precache-manifest.b0081dbf5d11b75fb3631b7ba2ebdfb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fd6071ca1cb6a76af988779984067e",
+    "url": "/Monster.inc/index.html"
+  },
+  {
+    "revision": "5ddc1877c075e5198a2c",
+    "url": "/Monster.inc/static/css/main.558ac0ff.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/Monster.inc/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster.inc/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddc1877c075e5198a2c",
+    "url": "/Monster.inc/static/js/main.723389b1.chunk.js"
+  },
+  {
+    "revision": "717511a3f633be6ba394",
+    "url": "/Monster.inc/static/js/runtime-main.36ede7bc.js"
+  }
+]);
